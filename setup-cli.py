328b4ad93dbb907ff01ce1122d8b17795f986499
Jay Clifford <jclifford@influxdata.com>
2023-04-13 18:15:48 +0100

    fixed readme error

--- setup-cli.py
+++ setup-cli.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 import re
 
-with open("../README.md", "r", encoding="utf-8") as fh:
+with open("./README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 def get_version_from_github_ref():
