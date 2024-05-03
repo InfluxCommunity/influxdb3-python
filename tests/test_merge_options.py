f0b3c03264fb18d283f4e91262f12037d6168fb6
Jakub Bednar <jakub.bednar@gmail.com>
2024-05-03 10:25:07 +0200

    feat: prepare to pass query parameters

---
+++ tests/test_merge_options.py
@@ -0,0 +1,28 @@
+import unittest
+
+import influxdb_client_3
+
+
+class MergeOptionsTests(unittest.TestCase):
+
+    def test_merge_with_empty_custom(self):
+        defaults = {"a": 1, "b": 2}
+        result = influxdb_client_3._merge_options(defaults, custom={})
+        self.assertEqual(result, defaults)
+
+    def test_merge_with_none_custom(self):
+        defaults = {"a": 1, "b": 2}
+        result = influxdb_client_3._merge_options(defaults, custom=None)
+        self.assertEqual(result, defaults)
+
+    def test_merge_with_no_excluded_keys(self):
+        defaults = {"a": 1, "b": 2}
+        custom = {"b": 3, "c": 4}
+        result = influxdb_client_3._merge_options(defaults, custom=custom)
+        self.assertEqual(result, {"a": 1, "b": 3, "c": 4})
+
+    def test_merge_with_excluded_keys(self):
+        defaults = {"a": 1, "b": 2}
+        custom = {"b": 3, "c": 4}
+        result = influxdb_client_3._merge_options(defaults, exclude_keys=["b"], custom=custom)
+        self.assertEqual(result, {"a": 1, "b": 2, "c": 4})
