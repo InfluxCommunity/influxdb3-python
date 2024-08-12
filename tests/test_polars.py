6a1426997d9306c17272558b2b2797735f2a8c92
Jakub Bednar <jakub.bednar@gmail.com>
2024-08-12 08:12:18 +0200

    fix: import PolarsDataframeSerializer

--- tests/test_polars.py
+++ tests/test_polars.py
@@ -1,10 +1,9 @@
-import unittest
 import importlib.util
+import unittest
 from unittest.mock import Mock
 
 from influxdb_client_3 import PointSettings, InfluxDBClient3
-from influxdb_client_3.write_client.client.write_api import WriteApi
-
+from influxdb_client_3.write_client import WriteService
 from influxdb_client_3.write_client.client.write.polars_dataframe_serializer import polars_data_frame_to_list_of_points
 
 
@@ -47,11 +46,10 @@ class TestWritePolars(unittest.TestCase):
     def test_write_polars(self):
         import polars as pl
         df = pl.DataFrame({
-            "time": ["2024-08-01 00:00:00", "2024-08-01 01:00:00"],
+            "time": pl.Series(["2024-08-01 00:00:00", "2024-08-01 01:00:00"]).str.to_datetime(time_unit='ns'),
             "temperature": [22.4, 21.8],
         })
-        mock_write = Mock(spec=WriteApi)
-        self.client._write_api.write = mock_write.write
+        self.client._write_api._write_service = Mock(spec=WriteService)
 
         self.client.write(
             database="database",
@@ -60,5 +58,6 @@ class TestWritePolars(unittest.TestCase):
             data_frame_timestamp_column="time",
         )
 
-        from polars.testing import assert_frame_equal
-        assert_frame_equal(df, self.client._write_api.write.call_args[1]['record'])
+        actual = self.client._write_api._write_service.post_write.call_args[1]['body']
+        self.assertEqual(b'measurement temperature=22.4 1722470400000000000\n'
+                         b'measurement temperature=21.8 1722474000000000000', actual)
