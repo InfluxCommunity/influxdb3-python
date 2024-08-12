0925d5c504b59f21d568f85dc2f5e75ffe723ead
Jakub Bednar <jakub.bednar@gmail.com>
2024-08-12 11:51:16 +0200

    chore(release): prepare for next development iteration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Change Log
 
+## 0.9.0 [unreleased]
+
 ## 0.8.0 [2024-08-12]
 
 ### Features
--- influxdb_client_3/version.py
+++ influxdb_client_3/version.py
@@ -1,4 +1,4 @@
 """Version of the Client that is used in User-Agent header."""
 
-VERSION = '0.8.0'
+VERSION = '0.9.0dev0'
 USER_AGENT = f'influxdb3-python/{VERSION}'
