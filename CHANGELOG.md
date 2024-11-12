bc48b1745393368417991a926b3c8d4c7aba1ae7
Tapanhaz <91151267+Tapanhaz@users.noreply.github.com>
2024-11-12 09:37:31 +0530

    fix: write polars dataframe (#113)
    * fix: write polars dataframe
    
    * docs: update CHANGELOG.md
    
    * chore: add tests
    
    * docs: Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Jakub Bednář <jakub.bednar@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## 0.10.0 [unreleased]
 
+### Bug Fixes
+
+1. [#113](https://github.com/InfluxCommunity/influxdb3-python/pull/113): Fix import error of `PolarsDataframeSerializer` in batching mode
+
 ## 0.9.0 [2024-09-13]
 
 ### Features
--- tests/test_polars.py
+++ tests/test_polars.py
@@ -2,7 +2,7 @@ import importlib.util
 import unittest
 from unittest.mock import Mock
 
-from influxdb_client_3 import PointSettings, InfluxDBClient3
+from influxdb_client_3 import PointSettings, InfluxDBClient3, write_client_options, WriteOptions
 from influxdb_client_3.write_client import WriteService
 from influxdb_client_3.write_client.client.write.polars_dataframe_serializer import polars_data_frame_to_list_of_points
 
@@ -49,6 +49,34 @@ class TestWritePolars(unittest.TestCase):
             "time": pl.Series(["2024-08-01 00:00:00", "2024-08-01 01:00:00"]).str.to_datetime(time_unit='ns'),
             "temperature": [22.4, 21.8],
         })
+        self.client._write_api._write_service = Mock(spec=WriteService)
+
+        self.client.write(
+            database="database",
+            record=df,
+            data_frame_measurement_name="measurement",
+            data_frame_timestamp_column="time",
+        )
+
+        actual = self.client._write_api._write_service.post_write.call_args[1]['body']
+        self.assertEqual(b'measurement temperature=22.4 1722470400000000000\n'
+                         b'measurement temperature=21.8 1722474000000000000', actual)
+
+    def test_write_polars_batching(self):
+        import polars as pl
+        df = pl.DataFrame({
+            "time": pl.Series(["2024-08-01 00:00:00", "2024-08-01 01:00:00"]).str.to_datetime(time_unit='ns'),
+            "temperature": [22.4, 21.8],
+        })
+        self.client = InfluxDBClient3(
+            host="localhost",
+            org="my_org",
+            database="my_db",
+            token="my_token", write_client_options=write_client_options(
+                write_options=WriteOptions(batch_size=2)
+            )
+        )
+        self.client._write_api._write_options = WriteOptions(batch_size=2)
         self.client._write_api._write_service = Mock(spec=WriteService)
 
         self.client.write(
