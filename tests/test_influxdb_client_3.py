299bc8374f81b0501f4ab0f545d22b4ffc533417
karel rehor <karl.koerner@bonitoo.io>
2025-03-18 17:22:01 +0100

    test: adds new unit test for client.query_async()

--- tests/test_influxdb_client_3.py
+++ tests/test_influxdb_client_3.py
@@ -2,6 +2,8 @@ import unittest
 from unittest.mock import patch
 
 from influxdb_client_3 import InfluxDBClient3
+from tests.util import asyncio_run
+from tests.util.mocks import ConstantFlightServer, ConstantData
 
 
 class TestInfluxDBClient3(unittest.TestCase):
@@ -47,6 +49,30 @@ class TestInfluxDBClient3(unittest.TestCase):
             auth_scheme="my_scheme"
         )
         self.assertEqual(client._client.auth_header_value, "my_scheme my_token")
+
+    @asyncio_run
+    async def test_query_async(self):
+        with ConstantFlightServer() as server:
+            client = InfluxDBClient3(
+                host=f"http://localhost:{server.port}",
+                org="my_org",
+                database="my_db",
+                token="my_token",
+            )
+
+            query = "SELECT * FROM my_data"
+
+            table = await client.query_async(query=query, language="sql")
+
+            result_list = table.to_pylist()
+
+            cd = ConstantData()
+            for item in cd.to_list():
+                assert item in result_list
+
+            assert {'data': 'database', 'reference': 'my_db', 'value': -1.0} in result_list
+            assert {'data': 'sql_query', 'reference': query, 'value': -1.0} in result_list
+            assert {'data': 'query_type', 'reference': 'sql', 'value': -1.0} in result_list
 
 
 if __name__ == '__main__':
