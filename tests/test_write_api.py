5fb1a493c143300dd05d08e5a6e70a93a06c4a17
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2025-05-09 18:42:04 +0700

    feat: support-env

---
+++ tests/test_write_api.py
@@ -0,0 +1,30 @@
+# tests/test_write_api.py
+
+import unittest
+from unittest import mock
+
+from influxdb_client_3.write_client.client.write_api import WriteApi
+from influxdb_client_3.write_client.domain import WritePrecision
+
+
+class TestWriteApi(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_client = mock.MagicMock()
+        self.mock_write_options = mock.Mock()
+        self.mock_point_settings = mock.Mock()
+        self.write_api = WriteApi(self.mock_client, self.mock_write_options, self.mock_point_settings)
+
+    @mock.patch("influxdb_client_3.write_client.client.write_api.WriteApi._write_batching")
+    def test_write_batching_with_bytes(self, mock_write_batching):
+        bucket = "my_bucket"
+        org = "my_org"
+        data = b"test_data"
+        precision = WritePrecision.NS
+
+        self.write_api._write_batching(bucket, org, data, precision)
+
+        mock_write_batching.assert_called_once_with(bucket, org, data, precision)
+
+
+
