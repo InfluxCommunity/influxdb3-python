c129e9deba05b6aa51462f3615e112619bddf7e4
Jan Simon <jan.simon@bonitoo.io>
2025-06-05 22:34:35 +0200

    chore: sync test name with file name

--- tests/test_write_local_server.py
+++ tests/test_write_local_server.py
@@ -8,7 +8,7 @@ from influxdb_client_3 import InfluxDBClient3, WriteOptions, WritePrecision, wri
 from influxdb_client_3.write_client.rest import ApiException
 
 
-class TestWriteWithLocalServer:
+class TestWriteLocalServer:
     SAMPLE_RECORD = "mem,tag=one value=1.0"
 
     @staticmethod
