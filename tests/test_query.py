ff136e14fe050ac6afe8c22f60173a295c8f03bb
karel rehor <karl.koerner@bonitoo.io>
2025-09-03 15:15:58 +0200

    tests: shorten test timeout

--- tests/test_query.py
+++ tests/test_query.py
@@ -445,7 +445,7 @@ Aw==
                     token=token,
                     flight_client_options={"generic_options": [('Foo', 'Bar')]},
                     proxy=None,
-                    options=QueryApiOptionsBuilder().timeout(0.001).build(),
+                    options=QueryApiOptionsBuilder().timeout(0.0001).build(),
                 )
                 query = "SELECT * FROM data"
                 await q_api.query_async(query, "sql", "", database)
