b1d07e72c5ecb6ad9ef93712e23928a3f3ed666f
Ales Pour <ales.pour@bonitoo.io>
2026-03-02 15:36:07 +0100

    test: more coverage

--- tests/test_point.py
+++ tests/test_point.py
@@ -1,6 +1,7 @@
 import datetime
 import unittest
 
+from influxdb_client_3 import WritePrecision
 from influxdb_client_3.write_client.client.write.point import EPOCH, Point
 
 
@@ -24,3 +25,33 @@ class TestPoint(unittest.TestCase):
         self.assertEqual('h2o,host=h1,rack=r1,region=us-east level=2i', point.to_line_protocol())
         self.assertEqual('h2o,region=us-east,host=h1,rack=r1 level=2i',
                          point.to_line_protocol(tag_order=["region", "", "host", "region", "missing"]))
+
+    def test_point_field_types_and_time_conversion(self):
+        point = Point.measurement("m") \
+            .field("drop", None) \
+            .field("flag", True) \
+            .field("name", "abc") \
+            .field("value", 1)
+
+        self.assertEqual('m flag=true,name="abc",value=1i', point.to_line_protocol())
+
+        dt = datetime.datetime(1970, 1, 1, 0, 0, 1, tzinfo=datetime.timezone.utc)
+        self.assertEqual('m value=1i 1000000000',
+                         Point.measurement("m").field("value", 1).time(dt, WritePrecision.NS).to_line_protocol())
+        self.assertEqual('m value=1i 1000000',
+                         Point.measurement("m").field("value", 1).time(dt, WritePrecision.US).to_line_protocol())
+        self.assertEqual('m value=1i 1000',
+                         Point.measurement("m").field("value", 1).time(dt, WritePrecision.MS).to_line_protocol())
+        self.assertEqual('m value=1i 1',
+                         Point.measurement("m").field("value", 1).time(dt, WritePrecision.S).to_line_protocol())
+        self.assertEqual('m value=1i 1000000',
+                         Point.measurement("m").field("value", 1)
+                         .time(datetime.timedelta(seconds=1), WritePrecision.US).to_line_protocol())
+        self.assertEqual('m value=1i 1',
+                         Point.measurement("m").field("value", 1)
+                         .time("1970-01-01T00:00:01Z", WritePrecision.S).to_line_protocol())
+
+        with self.assertRaisesRegex(ValueError, 'not supported'):
+            Point.measurement("m").field("bad", object()).to_line_protocol()
+        with self.assertRaises(ValueError):
+            Point.measurement("m").field("value", 1).time([]).to_line_protocol()
