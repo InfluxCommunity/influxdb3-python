d197f2357c99a106e272ec80ca49efe415f79bf3
karel rehor <karl.koerner@bonitoo.io>
2026-04-17 17:24:15 +0200

    chore: clean-up data updater.py

--- Examples/write/source_data/updater.py
+++ Examples/write/source_data/updater.py
@@ -1,14 +1,8 @@
 import pandas as pd
-import pyarrow.feather as feather
 import time
-from datetime import datetime, timedelta
 import logging
 import random
 import numpy as np
-import pyarrow as pa
-import pyarrow.compute as pac
-
-# TODO output as orc, json, csv etc.
 
 def update_measurement_name(source: pd.DataFrame, measurment_name: str):
     count = 0
@@ -18,29 +12,18 @@ def update_measurement_name(source: pd.DataFrame, measurment_name: str):
 
 def update_timestamps(source: pd.DataFrame):
 
-    #  now = datetime.now()
     now = time.time_ns();
     interval = 333_000_000 # ms
     grit = random.randrange(0, 1_000_000)
     interval = interval + grit
-    # current = now - (timedelta(milliseconds=interval) * len(source["time"]))
     current = np.int64(now - interval * len(source["time"]))
-    print(f"DEBUG source['time'] {source['time'][0]} {type(source['time'][0]).__name__}")
-    print(f"DEBUG source[\"time\"].dtype {source["time"].dtype}")
     ts_type = type(source['time'][0]).__name__
 
     count = 0
     for _ in source['time']:
         if ts_type in ["int", "int32", "int64"]:
-            #print(f"current {current} as type {type(current).__name__}")
             source.loc[count, "time"] = current
-            # source.at[count, "time"] = current
-            # source.at[count, "time"].astype('int64')
-            #print(f"DEBUG source.loc[count, \"time\"] {source.loc[count, "time"]} {type(source.loc[count, "time"]).__name__}")
-            # count += 1
         else:
-            # ts = pd.Timestamp(current)
-            # print(f"{data} {type(data)} ts {ts}")
             source.loc[count, "time"] = pd.Timestamp(current)
         count += 1
         current = current + interval
@@ -79,67 +62,7 @@ def json_update():
     logging.info("Updating json")
     json_df = pd.read_json('./out.json')
     update_timestamps(json_df)
-    print(f"DEBUG json_df[\"time\"][0] {json_df['time'][0]} as type {type(json_df['time'][0])}")
     json_df.to_json('./out_update.json', orient='records', index=False)
-
-def explore():
-    logging.info("Explore")
-    read_frame = feather.read_feather('./out.feather')
-    # print(f"DEBUG read_table:\n{read_table}")
-    c = len(read_frame["time"])
-    print(f"DEBUG c: {c}  type: {type(c)} \n")
-    print(f"DEBUG read_frame length: {read_frame.count()} type: {type(read_frame.count())}")
-    print(f"DEBUG read_frame:\n{read_frame}")
-
-    now = datetime.now()
-    interval = 334 # ms
-    # current = now - (timedelta(milliseconds=interval) * read_frame.count())
-    current = now - (timedelta(milliseconds=interval) * len(read_frame["time"]))
-
-    count = 0
-    # new_ts = []
-    for data in read_frame['time']:
-        print(f"{data} {type(data)} current {current}")
-        read_frame.loc[count, "time"] = current
-        # read_frame['time'][count] = data
-        # new_ts.append(current)
-        count += 1
-        current = current + timedelta(milliseconds=interval)
-
-    # for data in read_frame['time']:
-    #    print(f"{data}" )
-
-    print(f"DEBUG read_frame:\n{read_frame}")
-
-    # as feather
-    # feather.write_feather(read_frame, './out_test.feather')
-    read_frame.to_feather('./out_test.feather')
-
-    # as csv
-    count = 0
-    for _ in read_frame['iox::measurement']:
-        read_frame.loc[count, "iox::measurement"] = "machine_data_csv"
-        count += 1
-
-    read_frame.to_csv('./out_test.csv', columns=[
-        "iox::measurement","time","host","load","machineID","power","provider","temperature","topic","vibration"
-    ], index=False)
-
-    # as json
-    read_frame.to_json('./out_test.json', orient='records',)
-    # test_frame = read_frame.pivot(index='iox::measurement', columns='time', values=[
-    #    'iox::measurement','host','load','machineID','power','provider','temperature','topic','vibration'
-    # ])
-    # test_frame = pd.pivot_table(read_frame,values="iox::measurement", index="time", aggfunc=None)
-
-    # print(f"DEBUG test_frame:\n{test_frame}")
-    # test_frame.to_json('./out_test3.json')
-
-    # as orc
-    read_frame.to_orc('./out_test.orc')
-
-    test_frame = pd.read_csv('./out.csv')
-    print(f"DEBUG test_frame:\n{test_frame}")
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
